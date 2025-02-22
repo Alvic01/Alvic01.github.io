2f4310ebf2fa5d796944595d377b9f50e1e46b88
alice <adelheid@adelheid.lan>
2025-02-22 08:39:53 +0100

    Celtic quizz score

--- map/script.js
+++ map/script.js
@@ -1,5 +1,4 @@
 function checkAnswers() {
-    console.log("la fonction est bien éxécutée");
     let score = 0;
     const totalQuestions = 3;
 
@@ -10,18 +9,27 @@ function checkAnswers() {
 
     const q3 = document.querySelector('input[name="question3"]:checked');
 
+
+    console.log("q1 sélectionné :", q1);
+    console.log("q2 sélectionné :", q2);
+    console.log("q3 sélectionné :", q3);
+
+
     if (q1 && q1.value === "a") {
+        console.log("Bonne réponse pour question 1 !");
         score++;
     }
 
     if (q2 && q2.value === "c") {
+        console.log("Bonne réponse pour question 2 !");
         score++;
     }
 
     if (q3 && q3.value === "a") {
+        console.log("Bonne réponse pour question 3 !");
         score++;
     }
-
+    console.log("Final score :", score)
     const result = document.getElementById("result");
     result.innerHTML = `You have a total of ${score} correct answers out of 3.`;
 }
